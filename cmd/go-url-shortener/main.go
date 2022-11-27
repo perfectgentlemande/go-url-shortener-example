e931b4b37f688407069ab173026e34124272796b
Артем Чаадаев <perfectgentleman.de@ya.ru>
2022-11-28 02:34:21 +0400

    router added

--- cmd/go-url-shortener/main.go
+++ cmd/go-url-shortener/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/go-chi/chi"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/:url", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("resolve"))
+	})
+	r.Post("/api/v1", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("shorten"))
+	})
+	http.ListenAndServe(":3000", r)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/perfectgentlemande/go-url-shortener-example
 
 go 1.19
+
+require github.com/go-chi/chi v1.5.4
