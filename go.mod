0827bf2f58e98914dcfaaeb651ffeba8900736eb
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-02-16 02:10:52 +0400

    removed fiber responses

--- go.mod
+++ go.mod
@@ -20,8 +20,10 @@ require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
--- internal/api/shorten.go
+++ internal/api/shorten.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"errors"
 	"log"
 	"net/http"
@@ -11,41 +12,52 @@ import (
 	"github.com/perfectgentlemande/go-url-shortener-example/internal/service"
 
 	"github.com/asaskevich/govalidator"
-	"github.com/gofiber/fiber/v2"
 )
 
 func (c *Controller) Shorten(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	shortenReq := &ShortenRequest{}
 
-	if err := fCtx.BodyParser(&shortenReq); err != nil {
-		return fCtx.Status(fiber.StatusBadRequest).JSON(APIError{Message: "cannot parse JSON"})
+	if err := json.NewDecoder(r.Body).Decode(&shortenReq); err != nil {
+		log.Printf("wrong user data: %s\n", err)
+		WriteError(ctx, w, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+		return
 	}
 
 	// check if the input is an actual URL
 	if !govalidator.IsURL(shortenReq.Url) {
-		return fCtx.Status(fiber.StatusBadRequest).JSON(APIError{Message: "Invalid URL"})
+		log.Printf("invalid URL\n")
+		WriteError(ctx, w, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+		return
 	}
 
 	// check for domain error
 	if !helpers.RemoveDomainError(shortenReq.Url) {
-		return fCtx.Status(fiber.StatusBadRequest).JSON(APIError{Message: "Can't do that :)"})
+		log.Printf("can't do that :)\n")
+		WriteError(ctx, w, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+		return
 	}
 
 	newID, remainingQuota, limit, err := c.srvc.Shorten(ctx, fCtx.IP(), shortenReq.Url, shortenReq.Short, shortenReq.Expiry)
 	if err != nil {
 		log.Printf("cannot shorten URL: %s\n", err)
 		if errors.Is(err, service.ErrRateLimitExceeded) {
-			return fCtx.Status(fiber.StatusTooManyRequests).JSON(APIError{Message: "rate limit exceeded"})
+			log.Printf("rate limit exceeded\n")
+			WriteError(ctx, w, http.StatusTooManyRequests, http.StatusText(http.StatusTooManyRequests))
+			return
 		}
 		if errors.Is(err, service.ErrAlreadyInUse) {
-			return fCtx.Status(fiber.StatusBadRequest).JSON(APIError{Message: "already in use"})
+			log.Printf("already in use\n")
+			WriteError(ctx, w, http.StatusBadRequest, "already in use")
+			return
 		}
 
-		return fCtx.Status(fiber.StatusInternalServerError).JSON(APIError{Message: "cannot shorten URL"})
+		log.Printf("cannot shorten URL: %s\n", err)
+		WriteError(ctx, w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
 	}
 
-	resp := ShortenResponse{
+	WriteSuccessful(ctx, w, ShortenResponse{
 		ShortenRequest: ShortenRequest{
 			Url:    shortenReq.Url,
 			Short:  os.Getenv("DOMAIN") + "/" + newID,
@@ -53,7 +65,5 @@ func (c *Controller) Shorten(w http.ResponseWriter, r *http.Request) {
 		},
 		RateLimitRemaining: int64(remainingQuota),
 		RateLimitReset:     int64(limit / time.Nanosecond / time.Minute),
-	}
-
-	return fCtx.Status(fiber.StatusOK).JSON(resp)
+	})
 }
