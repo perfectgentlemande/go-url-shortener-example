20cbdf04fd5a8f1e6149eb0c5fb2111fb03e7bf0
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-03-11 20:06:22 +0400

    removed package log

--- internal/api/shorten.go
+++ internal/api/shorten.go
@@ -3,7 +3,6 @@ package api
 import (
 	"encoding/json"
 	"errors"
-	"log"
 	"net/http"
 	"os"
 	"time"
@@ -16,43 +15,45 @@ import (
 
 func (c *Controller) Shorten(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
+	log := GetLogger(ctx)
 	shortenReq := &ShortenRequest{}
 
 	if err := json.NewDecoder(r.Body).Decode(&shortenReq); err != nil {
-		log.Printf("wrong user data: %s\n", err)
+		log.WithError(err).Debug("wrong user data")
 		WriteError(ctx, w, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
 		return
 	}
+	log = log.WithField("url", shortenReq.Url)
 
 	// check if the input is an actual URL
 	if !govalidator.IsURL(shortenReq.Url) {
-		log.Printf("invalid URL\n")
+		log.Debug("invalid URL")
 		WriteError(ctx, w, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
 		return
 	}
 
 	// check for domain error
 	if !helpers.RemoveDomainError(shortenReq.Url, c.domain) {
-		log.Printf("can't do that :)\n")
+		log.Debug("can't do that :)")
 		WriteError(ctx, w, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
 		return
 	}
 
 	newID, remainingQuota, limit, err := c.srvc.Shorten(ctx, r.RemoteAddr, shortenReq.Url, shortenReq.Short, time.Duration(shortenReq.Expiry))
 	if err != nil {
-		log.Printf("cannot shorten URL: %s\n", err)
+
 		if errors.Is(err, service.ErrRateLimitExceeded) {
-			log.Printf("rate limit exceeded\n")
+			log.WithError(err).Debug("rate limit exceeded")
 			WriteError(ctx, w, http.StatusTooManyRequests, http.StatusText(http.StatusTooManyRequests))
 			return
 		}
 		if errors.Is(err, service.ErrAlreadyInUse) {
-			log.Printf("already in use\n")
+			log.WithError(err).Debug("already in use")
 			WriteError(ctx, w, http.StatusBadRequest, "already in use")
 			return
 		}
 
-		log.Printf("cannot shorten URL: %s\n", err)
+		log.WithError(err).Error("cannot shorten URL")
 		WriteError(ctx, w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
 	}
